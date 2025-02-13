39102f23bce18ea222c6a920b3f8d611f879001d
Joshua_vasquez <vas23220@uvg.edu.gt>
2025-02-13 14:17:16 -0600

    LAB 1 TERMINADO
    Se logro la ardua tarea de terminar el primer lab felicito a mis padres por el apoyo brindado y a mi auxiliar bryan morales uwu

--- Lab1/Prelab1/Prelab1/main.asm
+++ Lab1/Prelab1/Prelab1/main.asm
@@ -29,11 +29,16 @@ OUT SPH, R16
 ;CONFIGURACION 
 ;***********************************************
 SETUP:
+	//CONFIGURAMOS EL PRESCALER PRINCIPAL
+	LDI		R16, (1 << CLKPCE)		// Habilitar cambio de prescaler
+	STS		CLKPR, R16
+	LDI		R16, (1 << CLKPS2)
+	STS		CLKPR, R16				// CONFIGURAMOS PRESCALER A 16 F_CPU = 1Mhz
+
 	//Configuracion de pines de entrada y salida (DDRx, Portx, PINX)
 	// PORTD con entrada pull-up habilitado 
 	LDI		R16, 0xFF		 //Configuramos el purto D como salida
 	OUT		DDRD, R16
-	
 	
 	LDI		R16, 0xFF		 //Configuramos el purto C como salida
 	OUT		DDRC, R16
@@ -52,11 +57,12 @@ SETUP:
 
 	// Usamos R20 como contador
 	LDI R20, 0x00
+
 ;********************************************
 ;LOOP INFINITO 
 ;******************************************
 void_loop:
-	IN		R16, PINB		//Revisamos si el el boton fue presionado
+	IN		R16, PINB		//Revisamos si el el boton fue presionad
 	CP		R17, R16		// Si no hay cambio pasamos a void de nuevo
 	BREQ	void_loop
 	CALL	DELAY			// si hay cambio nos saltamos a BREQ y hacemos el delay
@@ -74,8 +80,9 @@ void_loop:
 	CALL	PINNC
 	SBRS	R16, 1
 	CALL	PINNC
-
-
+	SBRS	R16, 4
+	CALL	SUMADOR 
+	CALL	UPDATE_PORTD
 	RJMP	void_loop	
 		
 
@@ -85,24 +92,24 @@ PINNC:
 	CALL	DECREMENTO	// si el boton esta en set saltamos 
 	SBRS	R16, 1	// Este pin sera el boton de aumentar
 	CALL	AUMENTO	// llamamos a aumentar
-	RJMP	void_loop
+	RET
 
 
 DECREMENTO:
 	CPI		R20, 0x00	// Comparamos si es igual a 0 no hacemos nada
 	BREQ	void_loop	
 	dec		r20
-	out		PORTC,	r20
+	out		PORTD,	r20
 	ret
 
 AUMENTO:
-	CPI	R20, 0x0F		// Comparamos para ver si es 15 si lo es entonces saltamos
-	BREQ RESET		// reseteamos el registro
-	inc r20
-	out	PORTC, R20
+	CPI		R20, 0x0F		// Comparamos para ver si es 15 si lo es entonces saltamos
+	BREQ	RESET		// reseteamos el registro
+	inc		r20
+	out		PORTD, R20
 	RET
 
-	RESET:
+RESET:
 	LDI	R20, 0x00		// reseteamos el registro
 	RJMP void_loop			// regresamos a main
 
@@ -112,26 +119,47 @@ PINND:
 	CALL	DECREMENTAR	// si el boton esta en set saltamos 
 	SBRS	R16, 3	// Este pin sera el boton de aumentar
 	CALL	AUMENTAR	// llamamos a aumentar
-	RJMP	void_loop
+	RET
 
 
 DECREMENTAR:
-	CPI		R19, 0x00	// Comparamos si es igual a 0 no hacemos nada
+	CPI		R19, 0x0F	// Comparamos si es igual a 0 no hacemos nada
 	BREQ	void_loop	
 	dec		r19
-	out		PORTD,	r19
+	out		PORTD, r21
 	ret
 
 AUMENTAR:
-	CPI	R19, 0x0F		// Comparamos para ver si es 15 si lo es entonces saltamos
-	BREQ RESETEO		// reseteamos el registro
-	inc r19
-	out	PORTD, R19
+	CPI		R19, 0xFF		// Comparamos para ver si es 15 si lo es entonces saltamos
+	BREQ	RESETEO
+	inc		r19
+	CALL	UPDATE_PORTD
 	RET
 
 RESETEO:
 	LDI	R19, 0x00		// reseteamos el registro
 	RJMP void_loop			// regresamos a main
+
+UPDATE_PORTD:
+	MOV		R21, R19
+	ANDI	R21, 0x0F
+    SWAP    R21            
+	OR		R21, R20
+	OUT		PORTD, R21
+    RET
+
+SUMADOR: 
+    LDI     R22, 0x00 
+    MOV     R22, R19
+    ANDI    R22, 0x0F    
+    ANDI    R20, 0x0F    
+    ADD     R22, R20     
+
+    ; Guardar el resultado antes de modificar PORTC
+       
+    OUT     PORTC, R22  
+
+    RET
 
 DELAY:
 	LDI		R18, 0xFF
