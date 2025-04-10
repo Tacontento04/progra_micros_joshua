8489ff133aa2b8175a312c0901be54ae2d452d92
Joshua_vasquez <vas23220@uvg.edu.gt>
2025-04-10 00:40:00 -0600

    POSTLAB4 terminado

--- Ejemplos  en clase CC/Prelab4/main.c
+++ Ejemplos  en clase CC/Prelab4/main.c
@@ -4,23 +4,45 @@
 //Test.asm
 //
 //Autor: Joshua Vásquez
-//Proyecto: Proyecto 1
+//Proyecto: LAB4
 //Hardware: ATMega328P
-//Creado: 01/03/2025
+//Creado: 4/09/25
 //Modificado :
-//Descripcion: Es un reloj digital
+//Descripcion: ADC ( AD carry)
 
 //**************************************
 //ENCABEZADO
 //******************************************
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#define F_CPU 16000000
 
 // Variables globales
 volatile uint8_t counter = 0;
 volatile uint8_t debounce_counters[2] = {0, 0};
 volatile uint8_t last_state = 0x03;
 volatile uint8_t mux_state = 0;  // Estado del multiplexado (0, 1, 2)
+volatile uint8_t adc_value = 0;
+
+// Tabla hexadesimal
+const uint8_t tabla_7seg[16] = {
+	0x40, // 0
+	0x79, 
+	0x24, 
+	0x30, 
+	0x19, 
+	0x12, 
+	0x02, 
+	0x78, 
+	0x00, 
+	0x10, 
+	0x08, 
+	0x03, 
+	0x46, 
+	0x21, 
+	0x06, 
+	0x0E
+};
 
 void setup()
 {
@@ -29,12 +51,12 @@ void setup()
 	PORTD = 0x00;
 	
 	// 2. Configuración de botones
-	DDRC &= ~((1 << PC0) | (1 << PC1));
-	PORTC |= (1 << PC0) | (1 << PC1);
+	DDRC &= ~((1 << PORTB0) | (1 << PORTC1));
+	PORTC |= (1 << PORTC0) | (1 << PORTC1);
 	
 	// 3. Configuración de PORTB para los transistores (PB0, PB1, PB2 como salidas)
-	DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);
-	PORTB &= ~((1 << PB0) | (1 << PB1) | (1 << PB2)); // Todos apagados inicialmente
+	DDRB |= (1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2);
+	PORTB &= ~((1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2)); // Todos apagados inicialmente
 	
 	// 4. Timer0 para antirrebote (~10ms con 16MHz)
 	TCCR0A = 0x00;
@@ -47,10 +69,30 @@ void setup()
 	TIMSK2 = (1 << TOIE2);      // Habilitar interrupción por overflow
 	TCNT2 = 0;                  // Iniciar contador en 0
 	
+	// interrupcion del ADC
+	initADC();
+	ADCSRA	|= (1 << ADSC);
+
 	// 6. Estado inicial
 	counter = 0;
 	update_leds();
 	sei();
+}
+void initADC()
+{
+	ADMUX = 0;
+	ADMUX	|= (1<<REFS0);  // Se ponen los 5V como ref
+	//ADMUX &= ~(1<< REFS1);
+	ADMUX	|= (1 << ADLAR); // JUSTIF IZQ
+	ADMUX	|= (1 << MUX2) | (1<< MUX1); //Seleccionar el ADC6
+	// Por ultimo iniciar conversion
+	
+	ADCSRA	= 0;
+	ADCSRA	|= (1 << ADPS1) | (1 << ADPS0); // Frecuencia de muestreo de 125kHz
+	ADCSRA	|= (1 << ADIE); // Hab inter
+	ADCSRA	|= (1 << ADEN); //
+	
+	//ADCSRA	|= (1<< ADSC);
 }
 
 void update_leds()
@@ -64,24 +106,27 @@ int main(void)
 	
 	while (1)
 	{
-		// El trabajo principal se hace en las interrupciones
 	}
 }
 // Interrupción para multiplexado RÁPIDO (Timer2)
 ISR(TIMER2_OVF_vect) {
 	// Apagar todos los transistores primero
-	PORTB &= ~((1 << PB0) | (1 << PB1) | (1 << PB2));
+	PORTB &= ~((1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2));
 	
 	// Encender solo el transistor actual
 	switch(mux_state) {
-		case 0:
-		PORTB |= (1 << PB0);
+		case 0: // Decenas de segundos
+		PORTD = tabla_7seg[(adc_value >> 4) & 0x0F];
+		PORTB |= (1 << PORTB0);
 		break;
-		case 1:
-		PORTB |= (1 << PB1);
+		case 1: //Unidades de segundo 
+		PORTD = tabla_7seg[adc_value & 0x0F];
+		PORTB |= (1 << PORTB1);
 		break;
 		case 2:
-		PORTB |= (1 << PB2);
+		PORTD = 0X00;
+		update_leds();
+		PORTB |= (1 << PORTB2);
 		break;
 	}
 	
@@ -92,31 +137,46 @@ ISR(TIMER2_OVF_vect) {
 // Interrupción para antirrebote (Timer0 - original)
 ISR(TIMER0_OVF_vect)
 {
-	uint8_t current_state = PINC & ((1 << PC0) | (1 << PC1));
-	uint8_t changes = current_state ^ last_state;
-	last_state = current_state;
-	
-	// Procesar PC0 (incremento)
-	if (!(current_state & (1 << PC0))) {
-		debounce_counters[0]++;
-		if (debounce_counters[0] >= 5) {
-			counter++;
-			update_leds();
-			debounce_counters[0] = 0;
+	static uint8_t stable_state = 0x03;
+	uint8_t current_state = PINC & ((1 << PORTC0) | (1 << PORTC1));
+
+	// Botón PC0 - Incrementar
+	if (!(current_state & (1 << PORTC0))) {
+		if (++debounce_counters[0] >= 5) {
+			if (stable_state & (1 << PORTC0)) {
+				counter++;
+			}
+			stable_state &= ~(1 << PORTC0);
 		}
 		} else {
 		debounce_counters[0] = 0;
+		stable_state |= (1 << PORTC0);
 	}
-	
-	// Procesar PC1 (decremento)
-	if (!(current_state & (1 << PC1))) {
-		debounce_counters[1]++;
-		if (debounce_counters[1] >= 5) {
-			counter--;
-			update_leds();
-			debounce_counters[1] = 0;
+
+	// Botón PC1 - Decrementar
+	if (!(current_state & (1 << PORTC1))) {
+		if (++debounce_counters[1] >= 5) {
+			if (stable_state & (1 << PORTC1)) {
+				counter--;
+			}
+			stable_state &= ~(1 << PORTC1);
 		}
 		} else {
 		debounce_counters[1] = 0;
+		stable_state |= (1 << PORTC1);
 	}
 }
+
+ISR(ADC_vect)
+{
+	adc_value = ADCH; // Guardar el valor del ADC
+	// Comparar con el contador
+	if (adc_value > counter) {
+		PORTB |= (1 << PORTB3);   
+		} else {
+		PORTB &= ~(1 << PORTB3); 
+	}
+	ADCSRA	|= (1<< ADSC);
+}
+
+	
